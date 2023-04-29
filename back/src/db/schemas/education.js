50c68d3c019f5da64ce77c28b423e017ba3b5290
leewongyu <leewongyu@gmail.com>
2023-04-30 01:58:53 +0900

    fix: 현재 사용하지 않는 필드 제거

--- back/src/db/schemas/education.js
+++ back/src/db/schemas/education.js
@@ -1,36 +1,27 @@
 import mongoose, { Schema, model } from "mongoose";
-
 const EducationSchema = new Schema(
-  {
-    user: {
-      type: mongoose.Types.ObjectId,
-      ref: "User",
-      required: true,
+    {
+        user: {
+            type: mongoose.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        institution: {
+            type: String,
+            required: true,
+        },
+        degree: {
+            type: String,
+            required: true,
+        },
+        major: {
+            type: String,
+            required: true,
+        },
     },
-    institution: {
-      type: String,
-      required: true,
-    },
-    degree: {
-      type: String,
-      required: true,
-    },
-    major: {
-      type: String,
-      required: true,
-    },
-    startDate: {
-      type: Date,
-      required: true,
-    },
-    endDate: {
-      type: Date,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
 
 const EducationModel = model("education", EducationSchema);
