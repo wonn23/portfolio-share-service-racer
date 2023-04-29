7dafe5d32803a120ce32b1095651f97288ff9359
leewongyu <leewongyu@gmail.com>
2023-04-30 01:57:55 +0900

    fix: 작성되지 않은 함수 내용 제거

--- back/src/routers/awardRouter.js
+++ back/src/routers/awardRouter.js
@@ -19,39 +19,7 @@ awardRouter.use(tokenValidator);
  */
 awardRouter.post("/create", async function (req, res, next) {
   try {
-    const params = Object.values(req.body);
-    if (!validationParams(params)) {
-      console.log("비어있는 데이터가 존재합니다. 확인후 요청해주세요.");
-      res.status(404).send({
-        message: "비어있는 데이터가 존재합니다. 확인후 요청해주세요.",
-      });
-      return;
-    }
 
-    const { title, description } = req.body;
-
-    const user_id = req.currentUserId;
-    const user = User.findById({ user_id });
-    user.then((user) => {
-      if (!user) {
-        console.log("일치하는 유저가 없습니다.");
-        res.status(404).send({ message: "일치하는 유저가 없습니다." });
-        return;
-      }
-      const award = new AwardModel({
-        user: user._id,
-        title: title,
-        description: description,
-      });
-      const created = education.save();
-      if (!created) {
-        console.log("데이터베이스 입력에 실패했습니다.");
-        res.status(404).json({ message: "데이터베이스 입력에 실패했습니다." });
-        return;
-      }
-      console.log("데이터베이스 입력에 성공했습니다.");
-      res.status(200).json({ message: "데이터베이스 입력 되었습니다." });
-    });
   } catch (error) {
     next(error);
   }
