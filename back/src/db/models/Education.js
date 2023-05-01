4f6b84d9983f1ff472610552d5fbc9c975b5e80a
leewongyu <leewongyu@gmail.com>
2023-05-01 21:17:52 +0900

    fix: 불필요한 변수 사용된부분 삭제 및 수정

--- back/src/db/models/Education.js
+++ back/src/db/models/Education.js
@@ -1,64 +1,35 @@
 import { EducationModel } from "../schemas/education";
 
 class Education {
-  // education 생성
-  static async addEducation({ education }) {
-    return await EducationModel.create(education);
-  }
+    static async addEducation({education}) {
+        return EducationModel.create(education);
+    }
 
-  static async findById({ user_id }) {
-    const user = await EducationModel.findOne({ user: user_id });
-    return user;
-  }
+    static async findById({ educationId }) {
+        return EducationModel.findOne({ educationId: educationId });
+    }
 
-  static async findAll({ user_id }) {
-    const educations = await EducationModel.find({ user: user_id });
-    return educations;
-  }
+    static async findAll({ user_id }) {
+        return EducationModel.find({ user: user_id });
+    }
 
-  static async create({ newEducation }) {
-    const createdEducation = await EducationModel.create(newEducation);
-    return createdEducation;
-  }
+    static async update({ educationId, fieldToUpdate, newValue }) {
+        const filter = { educationId: educationId };
+        const update = { [fieldToUpdate]: newValue };
+        const option = { returnOriginal: false };
 
-  // education
-  static async findAllEduInfo({ email }) {
-    const educations = await EducationModel.find({ user: { email: email } });
-    return educations;
-  }
+        const updatedUser = await EducationModel.findOneAndUpdate(
+            filter,
+            update,
+            option
+        );
+        return updatedUser;
+    }
 
-  // education 수정
-  static async editEducation({ userId, educationId, education }) {
-    const filter = { user: userId, _id: educationId };
-    const update = { ...education };
-    const option = { new: true };
-    const editedEducation = await EducationModel.findOneAndUpdate(
-      filter,
-      update,
-      option
-    );
-    return editedEducation;
-  }
+    static async deleteById({ educationId }) {
+        return  EducationModel.deleteOne({ educationId: educationId });
+    }
 
-  static async update({ user_id, fieldToUpdate, newValue }) {
-    const filter = { _id: user_id };
-    const update = { [fieldToUpdate]: newValue };
-    const option = { returnOriginal: false };
-
-    const updatedUser = await EducationModel.findOneAndUpdate(
-      filter,
-      update,
-      option
-    );
-    return updatedUser;
-  }
-  // education 삭제
-  static async deleteById({ educationId }) {
-    const deletedEducation = await EducationModel.findByIdAndDelete(
-      educationId
-    );
-    return deletedEducation;
-  }
 }
 
 export { Education };
