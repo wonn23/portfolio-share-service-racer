cb177977605bb6f6880c89aafb6a4bdc7618f1a9
leewongyu <leewongyu@gmail.com>
2023-05-01 21:18:22 +0900

    fix: 필드 이름 수정 및 educationId 추가

--- back/src/db/schemas/education.js
+++ back/src/db/schemas/education.js
@@ -1,27 +1,32 @@
 import mongoose, { Schema, model } from "mongoose";
+
 const EducationSchema = new Schema(
-  {
-    user: {
-      type: mongoose.Types.ObjectId,
-      ref: "User",
-      required: true,
+    {
+        user: {
+            type: mongoose.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        educationId:{
+          type:String,
+          required:true,
+        },
+        school: {
+            type: String,
+            required: true,
+        },
+        major: {
+            type: String,
+            required: true,
+        },
+        status: {
+            type: String,
+            required: true,
+        },
     },
-    institution: {
-      type: String,
-      required: true,
-    },
-    degree: {
-      type: String,
-      required: true,
-    },
-    major: {
-      type: String,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
 
 const EducationModel = model("education", EducationSchema);
