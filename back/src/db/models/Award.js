5ce2a0442c98d619d7e353dabecec4f57d23e6d5
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-05-03 10:15:27 +0900

    chore: Award.js에서 await 생략 및 update함수 삭제, awardService에서 setAward 삭제

--- back/src/db/models/Award.js
+++ back/src/db/models/Award.js
@@ -6,37 +6,25 @@ class Award {
   }
 
   static async create({ newAward }) {
-    return await AwardModel.create(newAward);
+    return AwardModel.create(newAward);
   }
 
   static async findById({ user }) {
-    return await AwardModel.findOne({ id: user });
+    return AwardModel.findOne({ id: user });
   }
 
   static async findByUserId({ userId }) {
-    return await AwardModel.find({ userId });
+    return AwardModel.find({ userId });
   }
 
   static async findByIdAndUpdate({ _id }, update) {
-    return await AwardModel.findOneAndUpdate({ _id }, update, {
+    return AwardModel.findOneAndUpdate({ _id }, update, {
       new: true,
     });
   }
-  static async update({ user_id, fieldToUpdate, newValue }) {
-    const filter = { _id: user_id };
-    const update = { [fieldToUpdate]: newValue };
-    const option = { returnOriginal: false };
-
-    const updatedUser = await AwardModel.findOneAndUpdate(
-      filter,
-      update,
-      option
-    );
-    return updatedUser;
-  }
 
   static async deleteById({ awardId }) {
-    return await AwardModel.findByIdAndDelete(awardId);
+    return AwardModel.findByIdAndDelete(awardId);
   }
 }
 
--- back/src/services/awardService.js
+++ back/src/services/awardService.js
@@ -22,35 +22,6 @@ class AwardService {
     return await Award.findByUserId({ user_id });
   }
 
-  static async setAward({ award_id, toUpdate }) {
-    let award = await Award.findById(award_id);
-
-    // db에서 찾지 못한 경우, 에러 메시지 반환
-    if (!award) {
-      const errorMessage =
-        "해당 id를 가진 수상 데이터는 없습니다. 다시 한 번 확인해 주세요.";
-      return { errorMessage };
-    }
-
-    const fieldsToUpdate = {
-      title: "title",
-      description: "description",
-    };
-
-    for (const [field, fieldToUpdate] of Object.entries(fieldsToUpdate)) {
-      if (toUpdate[field]) {
-        const newValue = toUpdate[field];
-        award = await Award.update({
-          award_id,
-          fieldToUpdate,
-          newValue,
-        });
-      }
-    }
-
-    return award;
-  }
-
   static async updateAward({ _id, userId, toUpdate }) {
     const award = await Award.findById({ _id });
     if (!award) {
